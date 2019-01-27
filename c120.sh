63a399f1da9fdf8f90cbad1592dc688890b39ae7
Greg Hepworth <greg.hepworth@naimaudio.com>
2019-01-27 15:22:23 +0000

    Added script naem to echo's for easier identification

--- c120.sh
+++ c120.sh
@@ -11,22 +11,22 @@ do
 
     # if BASE_URL set then create an RSS feed from all downloaded episodes
     if [[ -z $BASE_URL ]]; then
-    	echo "No RSS base URL found, so not creating RSS feed"
+    	echo "c120.sh > No RSS base URL found, so not creating RSS feed"
     else
-    	echo "Creating RSS feed"
+    	echo "c120.sh > Creating RSS feed"
     	/usr/bin/get_iplayer_rss gen -d $CONFIG -o $OUTPUT -u $BASE_URL
     fi
     
     # if RSYNC_* set then sync downladed episodes to provided host
     if [[ -z $RSYNC_USER || -z $RSYNC_HOST || -z $RSYNC_PATH ]]; then
-    	echo "Complete rsync crednetials not found, so not attempting rysnc"
+    	echo "c120.sh > Complete rsync credentials not found, so not attempting rysnc"
     else
-    	echo "Attempting rsync"
+    	echo "c120.sh > Attempting rsync"
     	rsync -avzh /data/output/ $RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH
 	fi
 
 	# sleep for 4 hours
-	echo "Sleeping for 4 hours. zzz"
+	echo "c120.sh > Sleeping for 4 hours. zzz"
     sleep 14400
 done
 
